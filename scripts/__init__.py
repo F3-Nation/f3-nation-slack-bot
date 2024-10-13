540a2f3b8d2498bb37533756d85f7d1ba4b4fe78
Evan Petzoldt <evan.petzoldt@protonmail.com>
2024-10-13 07:41:05 -0500

    debugging scripts

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -10,16 +10,19 @@ def handle(request: Request) -> Response:
     decoded_data = request.data.decode()
     data_dict = json.loads(decoded_data)
     event_message = base64.b64decode(data_dict["message"]["data"]).decode()
+    print(f"Event message: {event_message}")
     try:
         if event_message == "hourly":
             print("Running hourly scripts")
             calendar_images.generate_calendar_images()
             return Response("Hourly scripts complete", status=200)
-        if event_message == "daily":
+        elif event_message == "daily":
             backblast_reminders.send_backblast_reminders()
             preblast_reminders.send_preblast_reminders()
             update_special_events.update_special_events()
             return Response("Daily scripts complete", status=200)
+        else:
+            return Response(f"Event message not used: {event_message}", status=200)
     except Exception as e:
         print(f"Error running scripts: {e}")
         return Response(f"Error: {e}", status=200)
