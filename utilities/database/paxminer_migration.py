3b276fd53dd4c41f248133821364196e7b5cdd56
Evan Petzoldt <evan.petzoldt@protonmail.com>
2025-08-24 08:16:22 -0500

    making some updates to paxminer migration scripts

--- utilities/database/paxminer_migration.py
+++ utilities/database/paxminer_migration.py
@@ -1,59 +1,39 @@
+import argparse
+import json
+import os
+import sys
+import uuid
+from datetime import date
 from typing import Dict, List
+
+from utilities.database.orm import SlackSettings
+from utilities.helper_functions import safe_get
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 from f3_data_models.models import (
     Attendance,
     Attendance_x_AttendanceType,
-    Event,
-    EventType_x_Event,
-    Org_x_SlackSpace,
-    SlackSpace,
+    EventInstance,
+    EventType_x_EventInstance,
+    Org,
+    Org_Type,
     SlackUser,
+    User,
 )
-from f3_data_models.utils import DbManager, get_engine
+from f3_data_models.utils import DbManager
 from sqlalchemy import text
 
-from utilities.database.orm import SlackSettings
-
-from .orm.paxminer import Attendance as PaxminerAttendance
-from .orm.paxminer import Backblast, PaxminerRegion
+from utilities.database.orm.paxminer import Attendance as PaxminerAttendance
+from utilities.database.orm.paxminer import Backblast, PaxminerRegion, PaxminerUser, get_pm_engine
 
 
-def convert_settings(slack_settings: SlackSettings, paxminer_region: PaxminerRegion) -> SlackSettings:
-    slack_settings.email_enabled = paxminer_region.email_enabled
-    slack_settings.email_server = paxminer_region.email_server
-    slack_settings.email_server_port = paxminer_region.email_server_port
-    slack_settings.email_user = paxminer_region.email_user
-    slack_settings.email_password = paxminer_region.email_password
-    slack_settings.email_to = paxminer_region.email_to
-    slack_settings.email_option_show = paxminer_region.email_option_show
-    slack_settings.postie_format = paxminer_region.postie_format
-    slack_settings.editing_locked = paxminer_region.editing_locked
-    slack_settings.default_backblast_destination = paxminer_region.default_destination
-    slack_settings.backblast_moleskin_template = paxminer_region.backblast_moleskin_template
-    slack_settings.preblast_moleskin_template = paxminer_region.preblast_moleskin_template
-    slack_settings.strava_enabled = paxminer_region.strava_enabled
-    slack_settings.custom_fields = paxminer_region.custom_fields
-    slack_settings.welcome_dm_enable = paxminer_region.welcome_dm_enable
-    slack_settings.welcome_dm_template = paxminer_region.welcome_dm_template
-    slack_settings.welcome_channel_enable = paxminer_region.welcome_channel_enable
-    slack_settings.welcome_channel = paxminer_region.welcome_channel
-    slack_settings.send_achievements = paxminer_region.send_achievements
-    slack_settings.send_aoq_reports = paxminer_region.send_aoq_reports
-    slack_settings.achievement_channel = paxminer_region.achievement_channel
-    slack_settings.default_siteq = paxminer_region.default_siteq
-    slack_settings.NO_POST_THRESHOLD = paxminer_region.NO_POST_THRESHOLD
-    slack_settings.REMINDER_WEEKS = paxminer_region.REMINDER_WEEKS
-    slack_settings.HOME_AO_CAPTURE = paxminer_region.HOME_AO_CAPTURE
-    slack_settings.NO_Q_THRESHOLD_WEEKS = paxminer_region.NO_Q_THRESHOLD_WEEKS
-    slack_settings.NO_Q_THRESHOLD_POSTS = paxminer_region.NO_Q_THRESHOLD_POSTS
-    return slack_settings
-
-
-def extract_name(backblast: str) -> str:
+def extract_name(backblast: str = "No title") -> str:
     # extract the name of the event from the backblast
     # this is a very basic implementation, and will need to be updated
     # to handle the various ways that backblasts are formatted
     # this is just a placeholder
+    backblast = backblast or "No title"
     name = backblast.split("\n")[0]
     if name:
         return name[:50]
@@ -61,37 +41,84 @@ def extract_name(backblast: str) -> str:
         return "Backblast"
 
 
-def convert_events(paxminer_backblasts: list[Backblast], slack_org_dict: Dict, region_org_id: int) -> List[Event]:
-    events: List[Event] = []
-    events = [
-        Event(
+def convert_region(paxminer_region: PaxminerRegion) -> Org:
+    return Org(
+        org_type=Org_Type.region,
+        name=paxminer_region.region,
+        is_active=True,
+    )
+
+
+def convert_users(paxminer_users: list[PaxminerUser], slack_team_id: str) -> List[SlackUser]:
+    users: List[User] = []
+    users = [
+        User(
+            f3_name=user.user_name,
+            first_name=user.real_name.split(" ")[0],
+            last_name=" ".join(user.real_name.split(" ")[1:]),
+            email=(user.user_id.lower() or uuid.uuid4()) if (user.email or "None") == "None" else user.email.lower(),
+            phone=user.phone,
+        )
+        for user in paxminer_users
+    ]
+    DbManager.create_or_ignore(User, users)
+    users = DbManager.find_records(User, filters=[True])
+    user_dict = {user.email: user.id for user in users}
+
+    slack_users: List[SlackUser] = []
+    for user in paxminer_users:
+        email = (user.user_id.lower() or uuid.uuid4()) if (user.email or "None") == "None" else user.email.lower()
+        slack_users.append(
+            SlackUser(
+                slack_id=user.user_id or uuid.uuid4(),
+                user_name=user.user_name,
+                email=email,
+                is_admin=False,
+                is_owner=False,
+                is_bot=user.app,
+                user_id=user_dict.get(email),
+                slack_team_id=slack_team_id,
+            )
+        )
+    return slack_users
+
+
+def convert_events(
+    paxminer_backblasts: list[Backblast], slack_org_dict: Dict, region_org_id: int
+) -> List[EventInstance]:
+    events: List[EventInstance] = []
+    for backblast in paxminer_backblasts:
+        if type(backblast.bd_date) is str:
+            backblast.bd_date = date(2020, 1, 1)
+    for backblast in paxminer_backblasts:
+        event = EventInstance(
             org_id=slack_org_dict.get(backblast.ao_id) or region_org_id,  # will return none if not found
-            is_series=False,
             is_active=True,
             highlight=False,
-            start_date=backblast.bd_date,
-            end_date=backblast.bd_date,
+            start_date=backblast.bd_date if backblast.bd_date > date(2020, 1, 1) else date(2020, 1, 1),
+            end_date=backblast.bd_date if backblast.bd_date > date(2020, 1, 1) else date(2020, 1, 1),
             name=extract_name(backblast.backblast),
             pax_count=backblast.pax_count,
             fng_count=backblast.fng_count,
             backblast=backblast.backblast_parsed,
-            meta=backblast.json,
-            backblast_ts=backblast.timestamp,
-            event_x_event_types=[EventType_x_Event(event_type_id=1)],  # can we assume a type based on ao name?
+            meta=json.loads(backblast.json or "{}"),
+            backblast_ts=None if backblast.timestamp or "" == "" else float(backblast.timestamp),
+            event_instances_x_event_types=[
+                EventType_x_EventInstance(event_type_id=1)  # assuming bootcamp
+            ],
         )
-        for backblast in paxminer_backblasts
-    ]
-
+        event.meta["source"] = "paxminer_import"
+        events.append(event)
     return events
 
 
-def get_event_id(event_lookup_dict: Dict, ao_id: str, date: str, q_user_id: str) -> int:
+def get_event_instance_id(event_lookup_dict: Dict, ao_id: str, date: str, q_user_id: str) -> int:
     if q_user_id[:5] == "https":
         q_user_id = q_user_id.split("/")[-1]
-    event_id = event_lookup_dict.get(f"{ao_id}-{date}-{q_user_id}")
-    # if not event_id:
+    event_instance_id = event_lookup_dict.get(f"{ao_id}-{date}-{q_user_id}")
+    # if not event_instance_id:
     #     raise ValueError(f"Event not found for {ao_id}-{date}-{q_user_id}")
-    return event_id
+    return event_instance_id
 
 
 def convert_attendance(
@@ -99,77 +126,80 @@ def convert_attendance(
 ) -> List[Attendance]:
     attendance_list: List[Attendance] = []
     for attendance in paxminer_attendance:
-        event_id = get_event_id(event_lookup_dict, attendance.ao_id, attendance.date, attendance.q_user_id)
-        if event_id:
+        event_instance_id = get_event_instance_id(
+            event_lookup_dict, attendance.ao_id, attendance.date, attendance.q_user_id
+        )
+        if event_instance_id:
             attendance_type_id = 2 if attendance.q_user_id == attendance.user_id else 1  # need to update for coqs
-            attendance_list.append(
-                Attendance(
-                    event_id=event_id,
-                    user_id=slack_user_dict.get(attendance.user_id),  # need to handle if user not found?
-                    attendance_x_attendance_types=[Attendance_x_AttendanceType(attendance_type_id=attendance_type_id)],
-                    is_planned=False,
+            user_id = slack_user_dict.get(attendance.user_id)
+            if user_id:  # this happens when the user_id is "https://", need to fix
+                attendance_list.append(
+                    Attendance(
+                        event_instance_id=event_instance_id,
+                        user_id=user_id,
+                        attendance_x_attendance_types=[
+                            Attendance_x_AttendanceType(attendance_type_id=attendance_type_id)
+                        ],
+                        is_planned=False,
+                        meta={"source": "paxminer_import"},
+                    )
                 )
-            )
-
+    # remove duplicates on event_instance_id, user_id, and is_planned
+    # TODO: this is a hack, need to fix the root cause
+    attendance_dict = {f"{a.event_instance_id}-{a.user_id}-{a.is_planned}": a for a in attendance_list}
+    attendance_list = list(attendance_dict.values())
     return attendance_list
 
 
-def build_event_lookup_dict(paxminer_backblasts: List[Backblast], events: List[Event]) -> Dict:
+def build_event_lookup_dict(paxminer_backblasts: List[Backblast], events: List[EventInstance]) -> Dict:
     event_lookup_dict = {}
     for i, backblast in enumerate(paxminer_backblasts):
         event_key = f"{backblast.ao_id}-{backblast.bd_date}-{backblast.q_user_id}"
         event_lookup_dict[event_key] = events[i].id
 
-    for i, backblast in enumerate(paxminer_backblasts):
-        if backblast.coq_user_id:
-            event_key = f"{backblast.ao_id}-{backblast.bd_date}-{backblast.coq_user_id}"
-            event_lookup_dict[event_key] = events[i].id
-
     return event_lookup_dict
 
 
-def run_paxminer_migration(from_pm_schema: str, slack_team_id: str):
-    # pull paxminer data
-    engine = get_engine(schema="paxminer", paxminer_db=True)
-    with engine.connect() as conn:
-        paxminer_region = conn.execute(text(f"SELECT * FROM regions WHERE schema_name = '{from_pm_schema}'")).fetchone()
-    paxminer_region = PaxminerRegion(**paxminer_region._mapping)
-    # pull region paxminer data
-    engine = get_engine(schema=from_pm_schema, paxminer_db=True)
+def run_paxminer_migration(region_org_id: int):
+    region_org_record = DbManager.get(Org, region_org_id, joinedloads=[Org.slack_space])
+    region_record = SlackSettings(**region_org_record.slack_space.settings)
+    engine = get_pm_engine(schema=region_record.paxminer_schema)
     with engine.connect() as conn:
         paxminer_backblasts = conn.execute(text("SELECT * FROM beatdowns")).fetchall()
         paxminer_attendance = conn.execute(text("SELECT * FROM bd_attendance")).fetchall()
-        # paxminer_aos = conn.execute(text("SELECT * FROM aos")).fetchall()
-    paxminer_backblasts = [Backblast(**backblast._mapping) for backblast in paxminer_backblasts]
-    paxminer_attendance = [PaxminerAttendance(**attendance._mapping) for attendance in paxminer_attendance]
-    # paxminer_aos = [PaxminerAO(**ao) for ao in paxminer_aos]
+    paxminer_backblasts = [
+        Backblast(**{k: v for k, v in backblast._mapping.items() if k in Backblast.__annotations__})
+        for backblast in paxminer_backblasts
+    ]
+    paxminer_attendance = [
+        PaxminerAttendance(**{k: v for k, v in attendance._mapping.items() if k in PaxminerAttendance.__annotations__})
+        for attendance in paxminer_attendance
+    ]
     engine.dispose()
 
-    # insert paxminer data
+    # pull ao records
+    aos = DbManager.find_records(Org, filters=[Org.parent_id == region_record.org_id, Org.org_type == Org_Type.ao])
+    slack_org_dict = {safe_get(ao.meta, "slack_channel_id"): ao.id for ao in aos if ao.meta}
+    # slack_org_dict.update(**AO_CHANNEL_MAP)
 
-    # region settings
-    slack_space: SlackSpace = DbManager.get(SlackSpace, slack_team_id)
-    slack_settings = SlackSettings(**slack_space.settings)
-    # slack_settings = convert_settings(slack_settings, paxminer_region)
-    # DbManager.update_record(
-    #     cls=SlackSpace, id=slack_space.team_id, fields={SlackSpace.settings: slack_settings.__dict__}
-    # )
-
-    # past events
-    org_x_slack_space: List[Org_x_SlackSpace] = DbManager.find_records(Org_x_SlackSpace, filters=[True])
-    slack_org_dict = {org.slack_id: org.org_id for org in org_x_slack_space}
-    events = convert_events(paxminer_backblasts, slack_org_dict, region_org_id=slack_settings.org_id)
-    print(len(events))
-    print(len(paxminer_backblasts))
-    events = DbManager.create_records(events)
-    event_lookup_dict = build_event_lookup_dict(paxminer_backblasts, events)
-    print(len(events))
-    print(len(event_lookup_dict))
-
-    # past attendance
-    slack_users: List[SlackUser] = DbManager.find_records(SlackUser, filters=[True])
+    # pull slack users
+    slack_users: List[SlackUser] = DbManager.find_records(
+        SlackUser, filters=[SlackUser.slack_team_id == region_record.team_id]
+    )
     slack_user_dict = {user.slack_id: user.user_id for user in slack_users}
+
+    # import past events
+    events = convert_events(paxminer_backblasts, slack_org_dict=slack_org_dict, region_org_id=region_record.org_id)
+    events: List[EventInstance] = DbManager.create_records(events)
+    event_lookup_dict = build_event_lookup_dict(paxminer_backblasts, events)
+
+    # import past attendance
     attendance = convert_attendance(paxminer_attendance, slack_user_dict, event_lookup_dict)
-    print(len(paxminer_attendance))
-    print(len(attendance))
     attendance = DbManager.create_records(attendance)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--region_org_id", help="The region org id to migrate to")
+    args = parser.parse_args()
+    run_paxminer_migration(int(args.region_org_id))
