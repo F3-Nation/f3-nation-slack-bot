1f0e0170c836e87eafe357e0807724f74f158322
Evan Petzoldt <evan.petzoldt@protonmail.com>
2025-11-21 13:05:21 +0000

    adding waiting and success forms for preblasts

--- features/calendar/__init__.py
+++ features/calendar/__init__.py
@@ -4,7 +4,3 @@ PREBLAST_MESSAGE_ACTION_ELEMENTS = [
     orm.ButtonElement(label=":hc: HC/Un-HC", action=actions.EVENT_PREBLAST_HC_UN_HC),
     orm.ButtonElement(label=":pencil: Edit Preblast", action=actions.EVENT_PREBLAST_EDIT, value="Edit Preblast"),
 ]
-
-PREBLAST_MESSAGE_ACTION_ELEMENTS_NO_Q = PREBLAST_MESSAGE_ACTION_ELEMENTS.insert(
-    0, orm.ButtonElement(label=":man_raising_hand: Take Q", action=actions.EVENT_PREBLAST_TAKE_Q)
-)
--- features/calendar/event_preblast.py
+++ features/calendar/event_preblast.py
@@ -34,7 +34,7 @@ from utilities.helper_functions import (
     safe_convert,
     safe_get,
 )
-from utilities.slack import actions, orm
+from utilities.slack import actions, forms, orm
 
 
 @dataclass
@@ -276,6 +276,23 @@ def handle_event_preblast_edit(
     metadata = json.loads(safe_get(body, "view", "private_metadata") or "{}")
     event_instance_id = safe_get(metadata, "event_instance_id")
     callback_id = safe_get(body, "view", "callback_id")
+
+    if (
+        form_data[actions.EVENT_PREBLAST_SEND_OPTIONS] == "Send now"
+        or callback_id == actions.EVENT_PREBLAST_POST_CALLBACK_ID
+        or (safe_get(metadata, "preblast_ts") or "None") != "None"
+    ):
+        preblast_send = True
+        forms.SUBMIT_FORM.update_modal(
+            client=client,
+            view_id=safe_get(body, "view", "id"),
+            callback_id="submit_form_waiting",
+            title_text="Submitting Preblast",
+            submit_button_text="None",
+        )
+    else:
+        preblast_send = False
+
     update_fields = {
         EventInstance.name: form_data[actions.EVENT_PREBLAST_TITLE],
         EventInstance.location_id: form_data[actions.EVENT_PREBLAST_LOCATION],
@@ -324,11 +341,7 @@ def handle_event_preblast_edit(
         ]
         DbManager.create_records(new_records)
 
-    if (
-        form_data[actions.EVENT_PREBLAST_SEND_OPTIONS] == "Send now"
-        or callback_id == actions.EVENT_PREBLAST_POST_CALLBACK_ID
-        or (safe_get(metadata, "preblast_ts") or "None") != "None"
-    ):
+    if preblast_send:
         send_preblast(
             body,
             client,
@@ -336,6 +349,13 @@ def handle_event_preblast_edit(
             context,
             region_record,
             event_instance_id,
+        )
+        forms.SUBMIT_FORM_SUCCESS.update_modal(
+            client=client,
+            view_id=safe_get(body, "view", "id"),
+            callback_id="submit_form_success",
+            title_text="Preblast Submitted",
+            submit_button_text="None",
         )
 
     # elif form_data[actions.EVENT_PREBLAST_SEND_OPTIONS] == "Schedule 24 hours before event":
