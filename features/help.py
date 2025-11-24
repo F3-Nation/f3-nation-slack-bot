b24cde25186afa03a181630fd8856635a0163de6
Evan Petzoldt <evan.petzoldt@protonmail.com>
2025-11-24 14:50:11 +0000

    added stats to response and now only visible to poster

--- features/help.py
+++ features/help.py
@@ -5,7 +5,9 @@ from logging import Logger
 from slack_sdk.models.blocks import ActionsBlock, ButtonElement, SectionBlock
 from slack_sdk.web import WebClient
 
+from features.user import IGNORE_EVENT
 from utilities.database.orm import SlackSettings
+from utilities.helper_functions import get_user, safe_get
 from utilities.slack import actions
 
 
@@ -54,36 +56,47 @@ def handle_app_mention(
     context: dict,
     region_record: SlackSettings,
 ):
+    slack_user_id = body["event"]["user"]
+    user_id = safe_get(get_user(slack_user_id, region_record, client, logger), "id")
+    blocks = [
+        SectionBlock(
+            text="Hi there! Looking for me? :wave:\n\nHere are some things I can help you with:",
+        ),
+        ActionsBlock(
+            elements=[
+                ButtonElement(
+                    text=":calendar: Open Calendar",
+                    action_id=actions.OPEN_CALENDAR_BUTTON,
+                ),
+                ButtonElement(
+                    text=":memo: Create Preblast",
+                    action_id=actions.PREBLAST_NEW_BUTTON,
+                ),
+                ButtonElement(
+                    text=":question: Help Menu",
+                    action_id=actions.CONFIG_HELP_MENU,
+                ),
+                ButtonElement(
+                    text=":gear: Settings",
+                    action_id=actions.SETTINGS_BUTTON,
+                ),
+            ]
+        ),
+    ]
+    if user_id:
+        blocks[1].elements.append(
+            ButtonElement(
+                text=":bar_chart: My Stats :link:",
+                url=f"{os.getenv('STATS_URL')}/stats/pax/{user_id}",
+                action=IGNORE_EVENT,
+            )
+        )
     try:
-        client.chat_postMessage(
+        client.chat_postEphemeral(
             channel=body["event"]["channel"],
+            user=body["event"]["user"],
             text="Hi there! Looking for me? :wave:",
-            blocks=[
-                SectionBlock(
-                    text="Hi there! Looking for me? :wave:\n\nHere are some things I can help you with:",
-                ),
-                ActionsBlock(
-                    elements=[
-                        ButtonElement(
-                            text=":calendar: Open Calendar",
-                            action_id=actions.OPEN_CALENDAR_BUTTON,
-                        ),
-                        ButtonElement(
-                            text=":memo: Create Preblast",
-                            action_id=actions.PREBLAST_NEW_BUTTON,
-                        ),
-                        ButtonElement(
-                            text=":question: Help Menu",
-                            action_id=actions.CONFIG_HELP_MENU,
-                        ),
-                        ButtonElement(
-                            text=":gear: Settings",
-                            action_id=actions.SETTINGS_BUTTON,
-                        ),
-                        # TODO: add a stats button that takes them to PAX Vault
-                    ]
-                ),
-            ],
+            blocks=blocks,
         )
     except Exception as e:
         logger.error(f"Error handling app mention: {e}")
