3abbe7c14167c1c59aeb6f6e1a17d9a10f896296
Evan Petzoldt <evan.petzoldt@protonmail.com>
2026-01-04 21:09:51 +0000

    fixing event type add menu - taking out 'select from global' selector, I think that was what is breaking it

--- features/calendar/event_type.py
+++ features/calendar/event_type.py
@@ -6,6 +6,7 @@ from typing import List
 from f3_data_models.models import Event_Category, EventType
 from f3_data_models.utils import DbManager
 from slack_sdk.web import WebClient
+from sqlalchemy import or_
 
 from utilities.builders import add_loading_form
 from utilities.database.orm import SlackSettings
@@ -38,25 +39,16 @@ def build_event_type_form(
 
     form = copy.deepcopy(EVENT_TYPE_FORM)
 
-    event_types_all: List[EventType] = DbManager.find_records(EventType, [EventType.is_active])
-    event_types_org = [
-        type.id
-        for type in event_types_all
-        if type.specific_org_id == region_record.org_id or type.specific_org_id is None
-    ]
-    event_types_other_org = [event_type for event_type in event_types_all if event_type.id not in event_types_org]
-    event_types_in_org = [event_type for event_type in event_types_all if event_type.id in event_types_org]
-    if not event_types_other_org:
-        form.blocks.pop(0)
-        form.blocks.pop(0)
-        form.blocks[0].label = "Create a new event type"
+    event_types_in_org: List[EventType] = DbManager.find_records(
+        EventType,
+        [
+            EventType.is_active,
+            or_(EventType.specific_org_id == region_record.org_id, EventType.specific_org_id.is_(None)),
+        ],
+    )
 
     form.set_options(
         {
-            actions.CALENDAR_ADD_EVENT_TYPE_SELECT: orm.as_selector_options(
-                names=[event_type.name for event_type in event_types_other_org],
-                values=[str(event_type.id) for event_type in event_types_other_org],
-            ),
             actions.CALENDAR_ADD_EVENT_TYPE_CATEGORY: orm.as_selector_options(
                 names=[c.name.capitalize() for c in Event_Category],
                 values=[c.name for c in Event_Category],
@@ -73,7 +65,6 @@ def build_event_type_form(
             }
         )
         form.blocks.pop(0)
-        # form.blocks.pop(0)
         form.blocks[0].label = "Edit Event Type"
         form.blocks[0].element.placeholder = "Edit Event Type"
         title_text = "Edit an Event Type"
@@ -107,7 +98,6 @@ def build_event_type_form(
 def handle_event_type_add(body: dict, client: WebClient, logger: Logger, context: dict, region_record: SlackSettings):
     form_data = EVENT_TYPE_FORM.get_selected_values(body)
     event_type_name = form_data.get(actions.CALENDAR_ADD_EVENT_TYPE_NEW)
-    event_type_id = form_data.get(actions.CALENDAR_ADD_EVENT_TYPE_SELECT)
     event_category = form_data.get(actions.CALENDAR_ADD_EVENT_TYPE_CATEGORY)
     event_type_acronym = form_data.get(actions.CALENDAR_ADD_EVENT_TYPE_ACRONYM)
     metadata = safe_convert(safe_get(body, "view", "private_metadata"), json.loads) or {}
@@ -124,17 +114,6 @@ def handle_event_type_add(body: dict, client: WebClient, logger: Logger, context
                 EventType.acronym: event_type_acronym or event_type.acronym,
                 EventType.specific_org_id: region_record.org_id,
             },
-        )
-
-    elif event_type_id:
-        event_type: EventType = DbManager.get(EventType, event_type_id)
-        DbManager.create_record(
-            EventType(
-                name=event_type.name,
-                event_category=event_type.event_category,
-                acronym=event_type.acronym,
-                specific_org_id=region_record.org_id,
-            )
         )
 
     elif event_type_name and event_category:
@@ -223,14 +202,7 @@ EVENT_TYPE_FORM = orm.BlockView(
             label="Note: Event Types are used to describe what you'll be doing at an event. They are different from Event Tags, which are used to give context to an event but do not change what you'll be doing at the event (e.g. 'VQ', 'Convergence', etc.).",  # noqa
         ),
         orm.InputBlock(
-            label="Select from commonly used event types",
-            element=orm.StaticSelectElement(placeholder="Select from commonly used event types"),
-            optional=True,
-            action=actions.CALENDAR_ADD_EVENT_TYPE_SELECT,
-        ),
-        orm.DividerBlock(),
-        orm.InputBlock(
-            label="Or create a new event type",
+            label="Create a new event type",
             element=orm.PlainTextInputElement(placeholder="New event type"),
             action=actions.CALENDAR_ADD_EVENT_TYPE_NEW,
             optional=True,
@@ -240,7 +212,7 @@ EVENT_TYPE_FORM = orm.BlockView(
             element=orm.StaticSelectElement(placeholder="Select an event category"),
             action=actions.CALENDAR_ADD_EVENT_TYPE_CATEGORY,
             optional=True,
-            hint="If entering a new event type, this is required for national aggregations (achievements, etc).",
+            hint="Tthis is required for national aggregations (achievements, etc).",
         ),
         orm.InputBlock(
             label="Event type acronym",
